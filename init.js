3160e2c8e67f8ccec8c4e16c788c2ee371860cad
javcof <javcof@qq.com>
2015-06-16 15:13:27 +0800

    ie dom loaded 事件处理

--- init.js
+++ init.js
@@ -14,8 +14,17 @@ var whenReady = (function() {
 	}
 	
 	if (document.addEventListener) {
+		// https://developer.mozilla.org/zh-CN/docs/Web/Events/DOMContentLoaded
 		document.addEventListener('DOMContentLoaded', handler, false);
+		window.addEventListener('load', handler, false);
+	} else if (document.attachEvent) {
+		// https://msdn.microsoft.com/library/ms536343%28v=vs.85%29.aspx
+		window.attachEvent('onload', handler);
 	}
+	
+	// https://developer.mozilla.org/zh-CN/docs/Web/API/GlobalEventHandlers/onload
+	window.onload = handler;
+	
 	return function whenReady(f) {
 		if (ready) f.call(document);
 		else funcs.push(f);
