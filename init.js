f040f68bc2d9307596ee1b9cad0a5d36c254e16c
javcof <javcof@qq.com>
2015-06-16 15:37:53 +0800

    模拟 DOMContentLoaded 事件

--- init.js
+++ init.js
@@ -1,9 +1,13 @@
-var whenReady = (function() {
+﻿var whenReady = (function() {
 	var funcs = [];
 	var ready = false;
 	
 	function handler(e) {
 		if (ready) return;
+		
+		// 模拟 DOMContentLoaded 事件
+		// https://developer.mozilla.org/zh-CN/docs/Web/Events/readystatechange
+		if (e.type === 'readystatechange' && document.readyState !== 'complete') return;
 		
 		for (var i = 0; i < funcs.length; i++) {
 			funcs[i].call(document);
@@ -16,8 +20,10 @@ var whenReady = (function() {
 	if (document.addEventListener) {
 		// https://developer.mozilla.org/zh-CN/docs/Web/Events/DOMContentLoaded
 		document.addEventListener('DOMContentLoaded', handler, false);
+		document.addEventListener('readystatechange', handler, false);
 		window.addEventListener('load', handler, false);
 	} else if (document.attachEvent) {
+		document.attachEvent('onreadystatechange', handler);
 		// https://msdn.microsoft.com/library/ms536343%28v=vs.85%29.aspx
 		window.attachEvent('onload', handler);
 	}
