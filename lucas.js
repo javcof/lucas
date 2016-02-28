c133f342d6b97143b32ccd2d190e76f2dc10f544
javcof <javcof@qq.com>
2016-02-28 22:17:08 +0800

    ie dom event fix

--- lucas.js
+++ lucas.js
@@ -59,7 +59,21 @@
 		} else if (element.attachEvent) {
 			// SD9011: 事件模型在各浏览器中存在差异
 			// http://www.w3help.org/zh-cn/causes/SD9011
-			element.attachEvent('on' + type, handler);
+			element.attachEvent('on' + type, function(e) {
+				e = e || window.event;
+				// https://developer.mozilla.org/zh-CN/docs/Web/API/Event/target
+				// 在 IE6-8 中，事件模型与标准不同。
+				// 使用非标准的 element.attachEvent() 方法绑定时间监听器。
+				// 在该模型中，事件对象有一个 srcElement 属性，等价于target 属性。
+				e.target = e.srcElement;
+				e.preventDefault = function() {
+					this.returnValue = false;
+				}
+				e.stopPropagation = function() {
+					this.cancelBubble = true;
+				}				
+				handler.call(element, e);
+			});
 		} else {
 			element['on' + type] = handler;
 		}
